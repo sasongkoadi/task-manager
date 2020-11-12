78d2c46a990e7fc483f4d5172533c206e9871fc0
SasongkoAdi <sasongko.adinugroho@qmail.id>
2020-11-12 19:11:15 +0800

    adding index.js for running server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/users', (req, res) => {
+  res.send('GET users request to the homepage')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
