9324620c6617d9565359e6615093c83eaa13503e
Sasongko Adi <sasongko.adinugroho@qmail.id>
2020-11-14 14:39:12 +0800

    connect to mongodb

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,47 +3,7 @@ const validator = require('validator').default
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 })
-
-
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    complete: {
-        type: Boolean,
-        default: false
-    }
-})
-
-/*
-const user = new User({
-    name: 'Zahra ',
-    age: 3,
-    email: ' ZAHRA27@qmail.id',
-    password: 'passWord' 
-})
-
-user.save().then((result) => {
-    console.log(result); 
-}).catch((err) => {
-    console.log(err.message); 
-});
-
-*/
-
-const task = new Task({
-    description: ' Learn Javascript'
-})
-
-task.save().then((task) => {
-    console.log(task);
-}).catch((error) => {
-    console.log(error.message);
-})
-
 
