39f4bd8757c7325e3ef6f34e29c8bb5185f90179
Sasongko Adi <sasongko.adinugroho@qmail.id>
2020-11-24 11:30:23 +0800

    add feature CRUD to users dan tasks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const { ObjectID } = require('mongodb')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -40,6 +41,34 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowUpdate = ['name', 'age', 'email', 'password']
+    const isUpdated = updates.every((update) => allowUpdate.includes(update))
+    if (!isUpdated) {
+        return res.status(400).send({
+            message: "Invalid Update"
+        })
+    }
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true})
+        !user? res.status(404).send({message: "User Not Found"}) : res.send(user)
+    } catch (error) {
+        res.status(500).send(error._message) 
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndDelete(_id) 
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(error._message)
+    }
+})
+
 //TASK 
 
 app.post('/tasks', async (req, res) => {
@@ -71,6 +100,31 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowUpdate = ['description', 'complete']
+    const isUpdated = updates.every((update) => allowUpdate.includes(update))
+    if (!isUpdated) {
+        return res.status(400).send({
+            message: 'Invalid Update'
+        })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})  
+        !task? res.status(404).send({message: "Task Not Found"}) : res.send(task)
+    } catch (error) {
+        res.status(500).send(error._message) 
+    }
+})
 
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        res.status(200).send( )
+    } catch (error) {
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
