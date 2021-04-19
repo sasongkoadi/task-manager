d6b51be3fca59a16bc114aaf8632e2a1ca61959c
Sasongko Adi <sasongko.adinugroho@qmail.id>
2021-04-19 11:05:14 +0800

    adding dockerfile

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ require('./db/mongoose')
 const usersRouter = require('./routers/users')
 const tasksRouter = require('./routers/task')
 const app = express()
-const port = process.env.PORT || 3000
+const HOST = '0.0.0.0'
+const PORT = process.env.PORT || 8080 
 
 app.use(express.json())
 app.use(usersRouter, tasksRouter)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT,HOST, () => console.log(`Example app listening on http://${HOST}:${PORT}`))
 
 const Task = require('./models/task')
 
